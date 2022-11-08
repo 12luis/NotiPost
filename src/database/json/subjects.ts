8f6fd829c916fc2954e14a958e09a33acc4385bc
saulmartinez.llevele <saulmartinez.llevele@gmail.com>
2022-11-08 16:25:03 -0600

    Fix on db

--- src/database/json/subjects.ts
+++ src/database/json/subjects.ts
@@ -3,7 +3,7 @@ export default [
         _id: "64b35bd513a72ab242f8a5b3",
         name: "Hipermedia",
         centerId: "63b66dc62428a5335238a5b1",
-        cve: "I5900",
+        cve: "I5910",
         root: true,
         active: true
     }
