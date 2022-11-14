779cd0e7f66f2718512022ad5ec05cfc2c13fdb1
saulmartinez.llevele <saulmartinez.llevele@gmail.com>
2022-11-13 23:51:54 -0600

    Posts advance

---
+++ src/database/json/posts.ts
@@ -0,0 +1,9 @@
+export default [
+    {
+        _id: "60f6414f8a21f5806755c282",
+        ownerId: "63a69ab53448a4224838a021",
+        title: 'ANUNCIO IMPORTANTE',
+        description: 'Los alumnos deben registrarse a...',
+        media: ''
+    }
+]
---
+++ src/routes/posts/model.ts
@@ -0,0 +1,64 @@
+import * as Mongoose from 'mongoose';
+import { model, Schema } from 'mongoose';
+
+const modelName = 'Post'
+
+const schema = new Schema({
+    _id: { 
+        type: Mongoose.Schema.Types.ObjectId, 
+        auto:true 
+    },
+    ownerId: {
+        type: Mongoose.Schema.Types.ObjectId,
+        required: true
+    },
+    title: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    media: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        required: true
+    },
+    finishAt: {
+        type: Date,
+        required: false
+    },
+    moment: {
+        type: Boolean,
+        required: true,
+    },
+    advised: {
+        type: Boolean,
+        required: true,
+        default: false
+    },
+    active: { 
+        type: Boolean, 
+        required: true, 
+        default: true 
+    },
+    deleted: { 
+        type: Boolean, 
+        required: true, 
+        default: false 
+    },
+    root: {
+        type: Boolean,
+        required: true,
+        default: false
+    }
+}, {
+    versionKey: false,
+    collection: modelName
+});
+
+export default model(modelName, schema);
